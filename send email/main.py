7d674185f8fe3d877b7ea3bc06b0092b308cf831
RickeyCore <kotovski.nichita@mail.ru>
2022-04-30 17:07:44 +0400

    Update

--- send email/main.py
+++ send email/main.py
@@ -1,6 +1,9 @@
 import smtplib
 import os
 from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
 from platform import python_version
 
 server = 'smtp.mail.ru'
@@ -16,6 +19,22 @@ html = '<html><head></head><body><p>' + text + '</p></body></html>'
 filepath = "fish.png"
 basename = os.path.basename(filepath)
 filesize = os.path.getsize(filepath)
+
+msg = MIMEMultipart('alternative')
+msg['Subject'] = subject
+msg['From'] = 'Python script <' + sender + '>'
+msg['To'] = ', '.join(recipients)
+msg['Reply-To'] = sender
+msg['Return-Path'] = sender
+msg['X-Mailer'] = 'Python/' + (python_version())
+
+part_text = MIMEText(text, 'plain')
+part_html = MIMEText(html, 'html')
+part_file = MIMEBase('application', 'octet-stream; name="{}"'.format(basename))
+part_file.set_payload(open(filepath, "rb").read())
+part_file.add_header('Content-Description', basename)
+part_file.add_header('Content-Disposition', 'attachment; filename="{}"; size={}'.format(basename, filesize))
+encoders.encode_base64(part_file)
 
 msg = MIMEMultipart('alternative')
 msg['Subject'] = subject
