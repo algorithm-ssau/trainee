bb68d1a513ca0c0e6424762bf0c19af8b4142ae3
RickeyCore <kotovski.nichita@mail.ru>
2022-05-18 21:36:01 +0400

    Update

--- send email/main.py
+++ send email/main.py
@@ -6,47 +6,47 @@ from email.mime.base import MIMEBase
 from email import encoders
 from platform import python_version
 
-server = 'smtp.mail.ru'
-user = 'info@test.org'
-password = 'Password'
 
-recipients = ['someuser1@mail.ru', 'someuser2@gmail.com']
-sender = 'info@test.org'
-subject = 'Тема сообщения 213213 777'
-text = 'Текст сообщения sdf sdf sdf sdaf <b>sdaf sdf</b> fg hsdgh <h1>f sd</h1> dfhjhgs sd gsdfg sdf'
-html = '<html><head></head><body><p>' + text + '</p></body></html>'
+def mail_send(to, text, theme):
+    server = 'smtp.mail.ru'
+    user = 'allen.night@mail.ru'
+    password = 'MpWEjJNwVssafaZrxeGp'
 
-filepath = "fish.png"
-basename = os.path.basename(filepath)
-filesize = os.path.getsize(filepath)
+    recipients = ['someuser1@mail.ru']
+    sender = 'info@test.org'
+    subject = 'Тема сообщения'
+    text = 'Здравствуйте, спасибо, что пользуетесь нашим сервисом'
+    html = '<html><head></head><body><p>' + text + '</p></body></html>'
 
-msg = MIMEMultipart('alternative')
-msg['Subject'] = subject
-msg['From'] = 'Python script <' + sender + '>'
-msg['To'] = ', '.join(recipients)
-msg['Reply-To'] = sender
-msg['Return-Path'] = sender
-msg['X-Mailer'] = 'Python/' + (python_version())
+    filepath = "fish.png"
+    basename = os.path.basename(filepath)
+    filesize = os.path.getsize(filepath)
 
-part_text = MIMEText(text, 'plain')
-part_html = MIMEText(html, 'html')
-part_file = MIMEBase('application', 'octet-stream; name="{}"'.format(basename))
-part_file.set_payload(open(filepath, "rb").read())
-part_file.add_header('Content-Description', basename)
-part_file.add_header('Content-Disposition', 'attachment; filename="{}"; size={}'.format(basename, filesize))
-encoders.encode_base64(part_file)
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = subject
+    msg['From'] = 'Python script <' + sender + '>'
+    msg['To'] = ', '.join(recipients)
+    msg['Reply-To'] = sender
+    msg['Return-Path'] = sender
+    msg['X-Mailer'] = 'Python/' + (python_version())
 
-msg = MIMEMultipart('alternative')
-msg['Subject'] = subject
-msg['From'] = 'Python script <' + sender + '>'
-msg['To'] = ', '.join(recipients)
-msg['Reply-To'] = sender
-msg['Return-Path'] = sender
-msg['X-Mailer'] = 'Python/' + (python_version())
+    part_text = MIMEText(text, 'plain')
+    part_html = MIMEText(html, 'html')
+    part_file = MIMEBase('application', 'octet-stream; name="{}"'.format(basename))
+    part_file.set_payload(open(filepath, "rb").read())
+    part_file.add_header('Content-Description', basename)
+    part_file.add_header('Content-Disposition', 'attachment; filename="{}"; size={}'.format(basename, filesize))
+    encoders.encode_base64(part_file)
 
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = subject
+    msg['From'] = 'Python script <' + sender + '>'
+    msg['To'] = ', '.join(recipients)
+    msg['Reply-To'] = sender
+    msg['Return-Path'] = sender
+    msg['X-Mailer'] = 'Python/' + (python_version())
 
-
-mail = smtplib.SMTP_SSL(server)
-mail.login(user, password)
-mail.sendmail(sender, recipients, msg.as_string())
-mail.quit()
+    mail = smtplib.SMTP_SSL(server)
+    mail.login(user, password)
+    mail.sendmail(sender, recipients, msg.as_string())
+    mail.quit()
