e709ab30b32ab63d2a06ed52e65a83ca35f1d4b1
SimpleNiK <41640209+SimpleNiK@users.noreply.github.com>
2022-05-21 10:36:15 +0400

    Add files via upload
    Добавил файл auth.js

---
+++ client/src/actions/auth.js
@@ -0,0 +1,11 @@
+export const signin = (formData, router) => async (dispatch) => {
+  try {
+    const { data } = await api.signIn(formData);
+
+    dispatch({ type: AUTH, data });
+
+    router.push('/');
+  } catch (error) {
+    console.log(error);
+  }
+};
